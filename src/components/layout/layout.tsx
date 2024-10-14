31d6c322a67be2150a61c58cb58b996a40bc576b
Jeeva Kiran <jadavani@miraclesoft.com>
2024-10-14 18:29:42 +0530

    Modified UI Sidebar and added logo

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,34 +2,39 @@ import React, { useState } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { Button } from 'primereact/button';
 import { Avatar } from 'primereact/avatar';
-import logo from '../../assets/applogo.jpg'
+import logo from '../../assets/onlyicon.png';
 import './layout.scss';
 
 const Layout = () => {
-    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
-    const [activePage, setActivePage] = useState('Home');
+    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
+    const [activePage, setActivePage] = useState('Dashboard');
 
     const loggedInUser = {
-        name: 'John Doe',
-        avatar: '/assets/user-avatar.png'
+        name: 'User',
+        avatar: 'https://images.miraclesoft.com/employee-profile-pics/kvedantam.png'
     };
 
     const menuItems = [
-        { label: 'Dashboard', icon: 'pi pi-home', to: '/home' }
+        { label: 'Dashboard', icon: 'pi pi-home', to: '/home' },
+        { label: 'Appointment', icon: 'pi pi-qrcode', to: '/home' },
+        { label: 'Settings', icon: 'pi pi-cog', to: '/home' },
+        { label: 'WareHouse', icon: 'pi pi-shop', to: '/home' },
     ];
 
-    const handleMenuClick = (label:any ) => {
+    const handleMenuClick = (label: string) => {
         setActivePage(label);
     };
 
     return (
         <div className="layout-wrapper">
             <aside className={`layout-sidebar ${isSidebarCollapsed ? 'collapsed' : ''}`}>
-                <Button 
-                    icon="pi pi-bars" 
-                    className="sidebar-toggle p-button-rounded p-button-text" 
-                    onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)} 
-                />
+                <div className="sidebar-header">
+                    <Button 
+                        icon="pi pi-bars" 
+                        className="sidebar-toggle p-button-rounded p-button-text" 
+                        onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)} 
+                    />
+                </div>
 
                 <div className="sidebar-menu">
                     {menuItems.map((item, index) => (
@@ -45,21 +50,21 @@ const Layout = () => {
                     ))}
                 </div>
 
-                <div className="sidebar-user">
-                    <Avatar image={loggedInUser.avatar} size="large" shape="circle" />
-                    {!isSidebarCollapsed && <span className="username">{loggedInUser.name}</span>}
+                <div className="sidebar-footer">
+                    <img src={logo} alt="Company Logo" className="sidebar-logo" />
                 </div>
-
-                <Link to="/logout" className={`sidebar-item logout-button ${isSidebarCollapsed ? 'collapsed' : ''}`}>
-                    <i className="pi pi-sign-out"></i>
-                    {!isSidebarCollapsed && <span className="logout-text" style={{marginLeft: '10px'}}>Logout</span>}
-                </Link>
             </aside>
 
             <div className="layout-main">
-                <header className="layout-header">
-                    {/* <img src={logo} alt="Company Logo" className="header-logo" /> */}
-                    {/* <h1 >{activePage}</h1> */}
+                <header className="layout-header" >
+                    <h1 className="header-title animated-text">{activePage}</h1>
+                    <div className="header-user">
+                        <span className="username">{loggedInUser.name}</span>
+                        <Avatar image={loggedInUser.avatar} size="large" shape="circle" className="avatar" />
+                        <Link to="/logout" className="logout-button">
+                            <i className="pi pi-sign-out"></i>
+                        </Link>
+                    </div>
                 </header>
                 <div className="layout-content">
                     <Outlet />
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,6 +6,39 @@ import "./home.scss";
 import { Orders } from "../services/Orders";
 
 const Home = () => {
+
+    const cardData = [
+        {
+          title: "Waiting",
+          value: 152,
+          icon: "pi pi-clock",
+          iconColor: "text-blue-500",
+          bgColor: "bg-blue-100",
+        },
+        {
+          title: "InBound",
+          value: 100,
+          icon: "pi pi-truck",
+          iconColor: "text-orange-500",
+          bgColor: "bg-orange-100",
+        },
+        {
+          title: "OutBound",
+          value: 28441,
+          icon: "pi pi-truck",
+          iconColor: "text-cyan-500",
+          bgColor: "bg-cyan-100",
+          mirrored: true,
+        },
+        {
+          title: "Appointment",
+          value: 152,
+          icon: "pi pi-bell",
+          iconColor: "text-purple-500",
+          bgColor: "bg-purple-100",
+        },
+      ];
+    
   const [customers, setCustomers] = useState([]);
 
   const paginatorLeft = <Button type="button" icon="pi pi-refresh" text />;
@@ -23,90 +56,38 @@ const Home = () => {
   };
   return (
     <div>
-      <h1>Dashboard</h1>
-
-      <div className="grid">
-        <div className="col-12 md:col-6 lg:col-3">
-          <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
-            <div className="flex justify-content-between mb-3">
-              <div>
-                <span className="block text-500 font-medium mb-3">Waiting</span>
-                <div className="text-900 font-medium text-xl">152</div>
-              </div>
-              <div
-                className="flex align-items-center justify-content-center bg-blue-100 border-round"
-                style={{ width: "2.5rem", height: "2.5rem" }}
-              >
-                <i className="pi pi-clock text-blue-500 text-xl"></i>
-              </div>
-            </div>
-           
-          </div>
-        </div>
-        <div className="col-12 md:col-6 lg:col-3">
-          <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
-            <div className="flex justify-content-between mb-3">
-              <div>
-                <span className="block text-500 font-medium mb-3">InBound</span>
-                <div className="text-900 font-medium text-xl">100</div>
-              </div>
-              <div
-                className="flex align-items-center justify-content-center bg-orange-100 border-round"
-                style={{ width: "2.5rem", height: "2.5rem" }}
-              >
-                <i className="pi pi-truck text-orange-500 text-xl"></i>
-              </div>
-            </div>
-          
-          </div>
-        </div>
-        <div className="col-12 md:col-6 lg:col-3">
+       <div className="grid custom-card">
+      {cardData.map((card, index) => (
+        <div className="col-12 md:col-6 lg:col-3" key={index}>
           <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
             <div className="flex justify-content-between mb-3">
               <div>
                 <span className="block text-500 font-medium mb-3">
-                  OutBound
+                  {card.title}
                 </span>
-                <div className="text-900 font-medium text-xl">28441</div>
+                <div className="text-900 font-medium text-xl">{card.value}</div>
               </div>
               <div
-                className="flex align-items-center justify-content-center bg-cyan-100 border-round"
+                className={`flex align-items-center justify-content-center ${card.bgColor} border-round custom-icon-card`}
                 style={{ width: "2.5rem", height: "2.5rem" }}
               >
-                <i className="pi pi-truck text-cyan-500 text-xl"></i>
+                <i className={`${card.icon} ${card.iconColor} text-xl`}
+                  style={card.mirrored ? { transform: "scaleX(-1)" } : {}}
+                >    
+                </i>
               </div>
             </div>
-           
           </div>
         </div>
-        <div className="col-12 md:col-6 lg:col-3">
-          <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
-            <div className="flex justify-content-between mb-3">
-              <div>
-                <span className="block text-500 font-medium mb-3">
-                  Appointment
-                </span>
-                <div className="text-900 font-medium text-xl">152 </div>
-              </div>
-              <div
-                className="flex align-items-center justify-content-center bg-purple-100 border-round"
-                style={{ width: "2.5rem", height: "2.5rem" }}
-              >
-                <i className="pi pi-bell text-purple-500 text-xl"></i>
-              </div>
-            </div>
-           
-          </div>
-        </div>
-      </div>
+      ))}
+    </div>
       <div className="card">
-  <div >
+  <div className="custom-scrollbar">
     <DataTable
       value={customers}
       paginator
       rows={5}
       rowsPerPageOptions={[5, 10, 25, 50]}
-     
       paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
       currentPageReportTemplate="{first} to {last} of {totalRecords}"
       paginatorLeft={paginatorLeft}
