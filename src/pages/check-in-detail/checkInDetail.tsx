9db7f0e6528643c4a865022d80cea54b58400b9d
PrasanthSankurabothu <psankurabothu@miraclesoft.com>
2024-10-16 19:20:41 +0530

    created detail checkin page

--- src/pages/check-in-detail/checkInDetail.tsx
+++ src/pages/check-in-detail/checkInDetail.tsx
@@ -1,8 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import "./checkInDetail.scss";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import { Image } from "primereact/image";
+import { Dropdown } from "primereact/dropdown";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import { Timeline } from "primereact/timeline";
+import { DropdownChangeEvent } from "primereact/dropdown";
 
+interface City {
+  name: string;
+  code: string;
+}
 // Define the types for your component's state (you can expand these as needed)
+interface TimelineEvent {
+  status?: string;
+  date?: string;
+  icon?: string;
+  color?: string;
+  image?: string;
+}
+
 interface DriverInfo {
   name: string;
   phoneNumber: string;
@@ -17,64 +35,132 @@ interface CheckInStatus {
   boundType: string;
 }
 export default function CheckInDetail() {
-  const {appointmentId} = useParams();
+  const { appointmentId } = useParams();
+  const [selectedCity, setSelectedCity] = useState<City | null>(null);
+  const cities: City[] = [
+    { name: "New York", code: "NY" },
+    { name: "Rome", code: "RM" },
+    { name: "London", code: "LDN" },
+    { name: "Istanbul", code: "IST" },
+    { name: "Paris", code: "PRS" },
+  ];
+  const events: TimelineEvent[] = [
+    {
+      status: "Ordered",
+      date: "15/10/2020 10:30",
+      icon: "pi pi-shopping-cart",
+      color: "#9C27B0",
+      image: "game-controller.jpg",
+    },
+    {
+      status: "Processing",
+      date: "15/10/2020 14:00",
+      icon: "pi pi-cog",
+      color: "#673AB7",
+    },
+    {
+      status: "Shipped",
+      date: "15/10/2020 16:15",
+      icon: "pi pi-shopping-cart",
+      color: "#FF9800",
+    },
+    {
+      status: "Delivered",
+      date: "16/10/2020 10:00",
+      icon: "pi pi-check",
+      color: "#607D8B",
+    },
+  ];
   const [driverInfo, setDriverInfo] = useState<DriverInfo>({
-    name: 'John Doe',
-    phoneNumber: '123-456-7890',
-    license: 'DL-987654321',
-    truckNo: 'TN-667788',
+    name: "John Doe",
+    phoneNumber: "123-456-7890",
+    license: "DL-987654321",
+    truckNo: "TN-667788",
     isVaccinated: true,
   });
-  const [status, setStatus] = useState<string>('WAITING');
-  const [boundType, setBoundType] = useState<string>('Outbound');
-  const [id, setId] = useState<number>(1); // Mock ID, in real-world scenario you might get this via props or routing
 
-  // Mock data fetching (replace with actual API call)
-  useEffect(() => {
-    // Simulate fetching driver info and check-in status
-    const fetchData = async () => {
-      // This would be an API call to fetch data
-      const fetchedDriverInfo = {
-        name: 'Prasanth Sankurabothu',
-        phoneNumber: '987-654-3210',
-        license: 'DL-987654321',
-        truckNo: 'TN-667788',
-        isVaccinated: true,
-      };
-      const fetchedStatus = 'LOADING'; // Example status
-      setDriverInfo(fetchedDriverInfo);
-      setStatus(fetchedStatus);
-    };
-    fetchData();
-  }, []);
-  useEffect(() => {
-    const fetchUserData = async () => {
-        const response = await fetch(`/api/users/${id}`); // Replace with your actual API endpoint
-        const data = await response.json();
-        console.log(data); // Handle your data (e.g., set it to state)
-    };
-
-    fetchUserData();
-}, [id]);
-  // Form submission handler
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('Form submitted with status:', status);
-
-    // Perform the update, possibly via an API call
-    // Example:
-    // await fetch('/api/updateStatus', { method: 'POST', body: JSON.stringify({ id, status }) });
   };
 
   return (
-  <div>
-    <h1>Driver Check-in</h1>
-    <div className="container">
-  <div className="item item1"></div>
-  <div className="item item2"></div>
-  <div className="item item3"></div>
-  <div className="item item4"></div>
-</div>
-  </div>
+    <>
+      <div className="grid-wrapper">
+        <div className="grid-item grid-item-1">
+          <div className="displayFlex-wrapper">
+            <div>
+              <h4>Driver Information</h4>
+              <div>
+                <p>Name: test</p>
+                <p>Phone: 2342342342</p>
+                <p>License: DL-2342342</p>
+                <p>Type: Inbound</p>
+              </div>
+            </div>
+            <div>
+              <h4>Appointment Information</h4>
+              <div>
+                <p>Appt Id: AP-5020</p>
+                <p>Status: AP DOWNLOAD-CMPLT</p>
+                <p>Date: 05-08-2024 10:45</p>
+                <p>Load No:</p>
+              </div>
+            </div>
+            <div>
+              <h4>Additional Information</h4>
+              <div>
+                <p>Test: test</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid-item grid-item-2">
+          <div className="info-box  border ">
+            <h4>Status</h4>
+            <div className="card flex justify-content-center">
+              <Dropdown
+                value={selectedCity}
+                onChange={(e: DropdownChangeEvent) => setSelectedCity(e.value)}
+                options={cities}
+                optionLabel="name"
+                placeholder="Select a City"
+                className="w-full md:w-14rem"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="grid-item grid-item-3">
+          <div className="timeline-card">
+            <Timeline value={events} content={(item) => item.status} />
+
+          </div>
+        </div>
+        {/* <div className="grid-item grid-item-4">Item 4</div> */}
+        <div className="grid-item grid-item-5">
+          <div className="displayFlex-wrapper">
+        <div>
+              <h4>Trip Information</h4>
+              <div>
+                <p>Name : Allentown PA</p>
+                <p>Id : 80502</p>
+                <p>ource System Code : ATL</p>
+              </div>
+            </div>
+            <div>
+              <h4>Customer</h4>
+              <div>
+                <p>Name : BUCHI KOMBUCHA</p>
+                <p>Status: AP DOWNLOAD-CMPLT</p>
+              
+              </div>
+            </div>
+            </div>
+         
+        </div>
+        <div className="grid-item grid-item-6">Item 6</div>
+      </div>
+    </>
   );
 }
--- src/pages/viewCheck_in/viewCheck_in.tsx
+++ src/pages/viewCheck_in/viewCheck_in.tsx
@@ -1,27 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { classNames } from 'primereact/utils';
-import { FilterMatchMode } from 'primereact/api';
-import { DataTable, DataTableFilterMeta } from 'primereact/datatable';
-import { Column, ColumnFilterElementTemplateOptions } from 'primereact/column';
-import { InputText } from 'primereact/inputtext';
-import { IconField } from 'primereact/iconfield';
-import { InputIcon } from 'primereact/inputicon';
-import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
-import { MultiSelect, MultiSelectChangeEvent } from 'primereact/multiselect';
-import { Tag } from 'primereact/tag';
-import { TriStateCheckbox, TriStateCheckboxChangeEvent } from 'primereact/tristatecheckbox';
-import { CustomerService } from '../services/Orders';
+import React, { useEffect, useState } from "react";
+import { classNames } from "primereact/utils";
+import { FilterMatchMode } from "primereact/api";
+import { DataTable, DataTableFilterMeta } from "primereact/datatable";
+import { Column, ColumnFilterElementTemplateOptions } from "primereact/column";
+import { InputText } from "primereact/inputtext";
+import { IconField } from "primereact/iconfield";
+import { InputIcon } from "primereact/inputicon";
+import { Dropdown, DropdownChangeEvent } from "primereact/dropdown";
+import { MultiSelect, MultiSelectChangeEvent } from "primereact/multiselect";
+import { Tag } from "primereact/tag";
+import {
+  TriStateCheckbox,
+  TriStateCheckboxChangeEvent,
+} from "primereact/tristatecheckbox";
+import { CustomerService } from "../services/Orders";
 // import "./viewCheck_in.scss";
-import { useNavigate } from 'react-router-dom';
-import { Button } from 'primereact/button';
+import { useNavigate } from "react-router-dom";
+import { Button } from "primereact/button";
 interface Representative {
   name: string;
   image: string;
 }
 
 interface Country {
-    name: string;
-    code: string;
+  name: string;
+  code: string;
 }
 
 interface Customer {
@@ -38,185 +41,297 @@ interface Customer {
 }
 
 export default function ViewCheck_in() {
-    const [customers, setCustomers] = useState<Customer[]>([]);
-    const [filters, setFilters] = useState<DataTableFilterMeta>({
-        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
-        'country.name': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
-        representative: { value: null, matchMode: FilterMatchMode.IN },
-        status: { value: null, matchMode: FilterMatchMode.EQUALS },
-        verified: { value: null, matchMode: FilterMatchMode.EQUALS }
+  const [customers, setCustomers] = useState<Customer[]>([]);
+  const [filters, setFilters] = useState<DataTableFilterMeta>({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    "country.name": { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    representative: { value: null, matchMode: FilterMatchMode.IN },
+    status: { value: null, matchMode: FilterMatchMode.EQUALS },
+    verified: { value: null, matchMode: FilterMatchMode.EQUALS },
+  });
+  const [loading, setLoading] = useState<boolean>(true);
+  const [globalFilterValue, setGlobalFilterValue] = useState<string>("");
+  const [representatives] = useState<Representative[]>([
+    { name: "Amy Elsner", image: "amyelsner.png" },
+    { name: "Anna Fali", image: "annafali.png" },
+    { name: "Asiya Javayant", image: "asiyajavayant.png" },
+    { name: "Bernardo Dominic", image: "bernardodominic.png" },
+    { name: "Elwin Sharvill", image: "elwinsharvill.png" },
+    { name: "Ioni Bowcher", image: "ionibowcher.png" },
+    { name: "Ivan Magalhaes", image: "ivanmagalhaes.png" },
+    { name: "Onyama Limba", image: "onyamalimba.png" },
+    { name: "Stephen Shaw", image: "stephenshaw.png" },
+    { name: "XuXue Feng", image: "xuxuefeng.png" },
+  ]);
+  const [statuses] = useState<string[]>(["Inbound", "Outbound"]);
+  const navigate = useNavigate();
+  const getSeverity = (status: string) => {
+    switch (status) {
+      case "Inbound":
+        return "danger";
+
+      case "Outbound":
+        return "success";
+
+      case "All":
+        return "info";
+    }
+  };
+
+  useEffect(() => {
+    CustomerService.getCustomersMedium().then((data: Customer[]) => {
+      setCustomers(getCustomers(data));
+      setLoading(false);
     });
-    const [loading, setLoading] = useState<boolean>(true);
-    const [globalFilterValue, setGlobalFilterValue] = useState<string>('');
-    const [representatives] = useState<Representative[]>([
-        { name: 'Amy Elsner', image: 'amyelsner.png' },
-        { name: 'Anna Fali', image: 'annafali.png' },
-        { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
-        { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
-        { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
-        { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
-        { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
-        { name: 'Onyama Limba', image: 'onyamalimba.png' },
-        { name: 'Stephen Shaw', image: 'stephenshaw.png' },
-        { name: 'XuXue Feng', image: 'xuxuefeng.png' }
-    ]);
-    const [statuses] = useState<string[]>(['Inbound','Outbound']);
-    const navigate = useNavigate();
-    const getSeverity = (status: string) => {
-        switch (status) {
-            case 'Inbound':
-                return 'danger';
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-            case 'Outbound':
-                return 'success';
+  const getCustomers = (data: Customer[]) => {
+    return [...(data || [])].map((d) => {
+      // @ts-ignore
+      d.date = new Date(d.date);
 
-            case 'All':
-                return 'info';
-        }
-    };
+      return d;
+    });
+  };
 
-    useEffect(() => {
-        CustomerService.getCustomersMedium().then((data: Customer[]) => {
-            setCustomers(getCustomers(data));
-            setLoading(false);
-        });
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  const onGlobalFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    let _filters = { ...filters };
 
-    const getCustomers = (data: Customer[]) => {
-        return [...(data || [])].map((d) => {
-            // @ts-ignore
-            d.date = new Date(d.date);
+    // @ts-ignore
+    _filters["global"].value = value;
 
-            return d;
-        });
-    };
+    setFilters(_filters);
+    setGlobalFilterValue(value);
+  };
 
-    const onGlobalFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        let _filters = { ...filters };
+  const renderHeader = () => {
+    return (
+      <div className="flex justify-content-end">
+        <IconField iconPosition="left">
+          <InputIcon className="pi pi-search" />
+          <InputText
+            value={globalFilterValue}
+            onChange={onGlobalFilterChange}
+            placeholder="Keyword Search"
+          />
+        </IconField>
+      </div>
+    );
+  };
 
-        // @ts-ignore
-        _filters['global'].value = value;
+  const countryBodyTemplate = (rowData: Customer) => {
+    return (
+      <div className="flex align-items-center gap-2">
+        <img
+          alt="flag"
+          src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png"
+          className={`flag flag-${rowData.country.code}`}
+          style={{ width: "24px" }}
+        />
+        <span>{rowData.country.name}</span>
+      </div>
+    );
+  };
 
-        setFilters(_filters);
-        setGlobalFilterValue(value);
-    };
-
-    const renderHeader = () => {
-        return (
-            <div className="flex justify-content-end">
-                <IconField iconPosition="left">
-                    <InputIcon className="pi pi-search" />
-                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
-                </IconField>
-            </div>
-        );
-    };
-
-    const countryBodyTemplate = (rowData: Customer) => {
-        return (
-            <div className="flex align-items-center gap-2">
-                <img alt="flag" src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png" className={`flag flag-${rowData.country.code}`} style={{ width: '24px' }} />
-                <span>{rowData.country.name}</span>
-            </div>
-        );
-    };
-
-    const representativeBodyTemplate = (rowData: Customer) => {
-        const representative = rowData.representative;
-
-        return (
-            <div className="flex align-items-center gap-2">
-                <img alt={representative.name} src={`https://primefaces.org/cdn/primereact/images/avatar/${representative.image}`} width="32" />
-                <span>{representative.name}</span>
-            </div>
-        );
-    };
-
-    const representativesItemTemplate = (option: Representative) => {
-        return (
-            <div className="flex align-items-center gap-2">
-                <img alt={option.name} src={`https://primefaces.org/cdn/primereact/images/avatar/${option.image}`} width="32" />
-                <span>{option.name}</span>
-            </div>
-        );
-    };
-
-    const statusBodyTemplate = (rowData: Customer) => {
-        return <Tag value={rowData.status} severity={getSeverity(rowData.status)} />;
-    };
-
-    const statusItemTemplate = (option: string) => {
-        return <Tag value={option} severity={getSeverity(option)} />;
-    };
-
-    const verifiedBodyTemplate = (rowData: Customer) => {
-        console.log(rowData);
-        navigate(`/home/check-in-detail/${rowData}`);
-    };
-
-    const representativeRowFilterTemplate = (options: ColumnFilterElementTemplateOptions) => {
-        return (
-            <MultiSelect
-                value={options.value}
-                options={representatives}
-                itemTemplate={representativesItemTemplate}
-                onChange={(e: MultiSelectChangeEvent) => options.filterApplyCallback(e.value)}
-                optionLabel="name"
-                placeholder="Any"
-                className="p-column-filter"
-                maxSelectedLabels={1}
-                style={{ minWidth: '14rem' }}
-            />
-        );
-    };
-
-    const statusRowFilterTemplate = (options: ColumnFilterElementTemplateOptions) => {
-        return (
-            <Dropdown value={options.value} options={statuses} onChange={(e: DropdownChangeEvent) => options.filterApplyCallback(e.value)} itemTemplate={statusItemTemplate} placeholder="Select One" className="p-column-filter" showClear style={{ minWidth: '12rem' }} />
-        );
-    };
-
-    const verifiedRowFilterTemplate = (options: ColumnFilterElementTemplateOptions) => {
-        return <TriStateCheckbox value={options.value} onChange={(e: TriStateCheckboxChangeEvent) => options.filterApplyCallback(e.value)} />;
-    };
-
-    const header = renderHeader();
+  const representativeBodyTemplate = (rowData: Customer) => {
+    const representative = rowData.representative;
 
     return (
-        <div className="card">
-        <DataTable 
-            value={customers} 
-            tableStyle={{ minWidth: '10rem' }}
-            paginator 
-            resizableColumns 
-            showGridlines
-            rows={7}
-            dataKey="id"
-            filters={filters}
-            filterDisplay="row"
-            loading={loading}
-            globalFilterFields={['name', 'country.name', 'representative.name', 'status']}
-            header={header}
-            emptyMessage="No customers found."
-            scrollable 
-            scrollHeight="flex"   
-            responsiveLayout="scroll"
-        >
-            <Column field="name" header="Driver" filter filterPlaceholder="Search by driver name" style={{ minWidth: '12rem' }} />
-            <Column header="Appointment #" filterField="country.name" style={{ minWidth: '12rem' }} body={countryBodyTemplate} filter filterPlaceholder="Search by appointment" className="appointment-column" />
-            <Column header="Type" filterField="representative" showFilterMenu={false} filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '14rem' }} 
-                body={representativeBodyTemplate} filter filterElement={representativeRowFilterTemplate} className="type-column" />
-            <Column field="status" header="Status" showFilterMenu={false} filterMenuStyle={{ width: '12rem' }} style={{ minWidth: '12rem' }} body={statusBodyTemplate} filter filterElement={statusRowFilterTemplate} className="status-column" />
-            <Column field="verified" header="Actions" dataType="boolean" style={{ minWidth: '6rem' }} 
-                body={(rowData) => (
-                    <Button label="View" className="bgColor" onClick={() => verifiedBodyTemplate(rowData.id)} />
-                )} 
-                filter filterElement={verifiedRowFilterTemplate} 
-            />
-        </DataTable>
-    </div>
-    
+      <div className="flex align-items-center gap-2">
+        <img
+          alt={representative.name}
+          src={`https://primefaces.org/cdn/primereact/images/avatar/${representative.image}`}
+          width="32"
+        />
+        <span>{representative.name}</span>
+      </div>
     );
+  };
+
+  const representativesItemTemplate = (option: Representative) => {
+    return (
+      <div className="flex align-items-center gap-2">
+        <img
+          alt={option.name}
+          src={`https://primefaces.org/cdn/primereact/images/avatar/${option.image}`}
+          width="32"
+        />
+        <span>{option.name}</span>
+      </div>
+    );
+  };
+
+  const statusBodyTemplate = (rowData: Customer) => {
+    return (
+      <Tag value={rowData.status} severity={getSeverity(rowData.status)} />
+    );
+  };
+
+  const statusItemTemplate = (option: string) => {
+    return <Tag value={option} severity={getSeverity(option)} />;
+  };
+
+  const verifiedBodyTemplate = (rowData: Customer) => {
+    console.log(rowData);
+    navigate(`/home/check-in-detail/${rowData}`);
+  };
+
+  const representativeRowFilterTemplate = (
+    options: ColumnFilterElementTemplateOptions
+  ) => {
+    return (
+      <MultiSelect
+        value={options.value}
+        options={representatives}
+        itemTemplate={representativesItemTemplate}
+        onChange={(e: MultiSelectChangeEvent) =>
+          options.filterApplyCallback(e.value)
+        }
+        optionLabel="name"
+        placeholder="Any"
+        className="p-column-filter"
+        maxSelectedLabels={1}
+        style={{ minWidth: "14rem" }}
+      />
+    );
+  };
+
+  const statusRowFilterTemplate = (
+    options: ColumnFilterElementTemplateOptions
+  ) => {
+    return (
+      <Dropdown
+        value={options.value}
+        options={statuses}
+        onChange={(e: DropdownChangeEvent) =>
+          options.filterApplyCallback(e.value)
+        }
+        itemTemplate={statusItemTemplate}
+        placeholder="Select One"
+        className="p-column-filter"
+        showClear
+        style={{ minWidth: "12rem" }}
+      />
+    );
+  };
+
+  const verifiedRowFilterTemplate = (
+    options: ColumnFilterElementTemplateOptions
+  ) => {
+    return (
+      <TriStateCheckbox
+        value={options.value}
+        onChange={(e: TriStateCheckboxChangeEvent) =>
+          options.filterApplyCallback(e.value)
+        }
+      />
+    );
+  };
+
+  const header = renderHeader();
+  const [isMobile, setIsMobile] = useState(false);
+  const [isTablet, setIsTablet] = useState(false);
+
+  // Update state based on screen size
+  const handleResize = () => {
+    const width = window.innerWidth;
+    setIsMobile(width <= 768);
+    setIsTablet(width > 768 && width <= 1024);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return (
+    <div className="card">
+      <DataTable
+        value={customers}
+        tableStyle={{ minWidth: "10rem" }}
+        paginator
+        resizableColumns
+        showGridlines
+        rows={7}
+        dataKey="id"
+        filters={filters}
+        filterDisplay="row"
+        loading={loading}
+        globalFilterFields={[
+          "name",
+          "country.name",
+          "representative.name",
+          "status",
+        ]}
+        header={header}
+        emptyMessage="No customers found."
+        scrollable
+        scrollHeight="flex"
+        responsiveLayout="scroll"
+      >
+        <Column
+          field="name"
+          header="Driver"
+          filter
+          filterPlaceholder="Search by driver name"
+          style={{ minWidth: "12rem" }}
+        />
+
+        {!isMobile && !isTablet && (
+          <Column
+            header="Appointment #"
+            filterField="country.name"
+            style={{ minWidth: "12rem" }}
+            body={countryBodyTemplate}
+            filter
+            filterPlaceholder="Search by appointment"
+          />
+        )}
+
+        {!isMobile && !isTablet && (
+          <Column
+            header="Type"
+            filterField="representative"
+            showFilterMenu={false}
+            filterMenuStyle={{ width: "14rem" }}
+            style={{ minWidth: "14rem" }}
+            body={representativeBodyTemplate}
+            filter
+            filterElement={representativeRowFilterTemplate}
+          />
+        )}
+
+        {!isMobile && (
+          <Column
+            field="status"
+            header="Status"
+            showFilterMenu={false}
+            filterMenuStyle={{ width: "12rem" }}
+            style={{ minWidth: "12rem" }}
+            body={statusBodyTemplate}
+            filter
+            filterElement={statusRowFilterTemplate}
+          />
+        )}
+
+        <Column
+          field="verified"
+          header="Actions"
+          dataType="boolean"
+          style={{ minWidth: "6rem" }}
+          body={(rowData) => (
+            <Button
+              label="View"
+              className="bgColor"
+              onClick={() => verifiedBodyTemplate(rowData.id)}
+            />
+          )}
+          filter
+          filterElement={verifiedRowFilterTemplate}
+        />
+      </DataTable>
+    </div>
+  );
 }
