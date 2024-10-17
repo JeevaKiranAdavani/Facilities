2e089e0130d7865f3a5f543f8688dcef45520c87
vineetha kondepudi <vineethakondepudi22@gmail.com>
2024-10-17 20:48:32 +0530

    Added driver status route

--- src/routes/routeConfig.tsx
+++ src/routes/routeConfig.tsx
@@ -4,6 +4,7 @@ import SignUp from '../pages/registration/registration';
 import CheckIn from '../pages/checkin/checkin';
 import ViewCheckIn from '../pages/viewCheck_in/viewCheck_in';
 import CheckInDetail from '../pages/check-in-detail/checkInDetail';
+import DriverStatus from '../pages/driver_status/driver_status';
 import Unauthorized from '../pages/unauthorized/unauthorized';
 
 const routeConfig = [
@@ -47,6 +48,13 @@ const routeConfig = [
       element: <CheckInDetail />,
       isProtected: true,
       allowedRoles: ["Manager"],
+      useLayout: true,
+    },
+    {
+      path: "/home/driverStatus",
+      element: <DriverStatus />,
+      isProtected: true,
+      allowedRoles: ["Driver","Manager"],
       useLayout: true,
     },
     {
